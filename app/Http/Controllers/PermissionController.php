7f48ea314160b801cfc0fc806bd4cf73e905d0ee
tokatu4561 <ryobsk4561@gmail.com>
2022-05-06 18:51:14 -0700

    role and permittion relation

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use Illuminate\Http\Request;
 
 class PermissionController extends Controller
 {
-    //
+    public function index()
+    {
+        return Permission::all();
+    }
 }
