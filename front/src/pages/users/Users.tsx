80b3a18b6cb27454b7a9930e13f5860ef71b5417
tokatu4561 <ryobsk4561@gmail.com>
2022-05-13 23:24:33 -0700

    add pagination in users page and feat delete user

--- front/src/pages/users/Users.tsx
+++ front/src/pages/users/Users.tsx
@@ -6,14 +6,41 @@ import { User } from "../../types/user";
 
 const Users = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lastPage, setLastPage] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const { data: usersData } = await axios.get("/users");
+      const { data: usersData } = await axios.get(`/users?page=${currentPage}`);
 
       setUsers(usersData.data);
+      setLastPage(usersData.meta.last_page);
     })();
-  }, []);
+  }, [currentPage]);
+
+  const onNext = () => {
+    if (currentPage < lastPage) {
+      setCurrentPage((page) => page + 1);
+    }
+  };
+
+  const onPrev = () => {
+    if (currentPage >= 1) {
+      setCurrentPage((page) => page - 1);
+    }
+  };
+
+  const handleDelUser = async (id: number) => {
+    if (window.confirm("本当に削除しますか？")) {
+      try {
+        await axios.delete(`users/${id}`);
+
+        setUsers((currentUsers) => {
+          return currentUsers.filter((u: User) => u.id !== id);
+        });
+      } catch (error) {}
+    }
+  };
 
   return (
     <Layout>
@@ -44,12 +71,39 @@ const Users = () => {
                 </th>
                 <th>{user.email}</th>
                 <th>{user.role.name}</th>
-                <th></th>
+                <th>
+                  <div>
+                    <a
+                      href="#"
+                      className="btn btn-sm btn-outline-secondary"
+                      onClick={() => {
+                        handleDelUser(user.id);
+                      }}
+                    >
+                      Delete
+                    </a>
+                  </div>
+                </th>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+
+      <nav>
+        <ul className="pagination">
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={onPrev}>
+              Previous
+            </a>
+          </li>
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={onNext}>
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
     </Layout>
   );
 };
